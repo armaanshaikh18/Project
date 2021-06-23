5084bcf780eb988ef3fbdbaed94fe766381b36c4
Armaan <atteshamshaikh47@gmail.com>
2021-06-23 20:10:40 +0530

    First Comment
    My First Repository

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import React,{useState} from 'react';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
+import HeroSection from '../components/HeroSection';
+import InfoSection from '../components/InfoSection';
+import { homeObjOne } from '../components/InfoSection/Data';
 const Home = () => {
    const [isOpen, setIsOpen] = useState(false)
 
@@ -13,7 +16,8 @@ const Home = () => {
         <>
             <Sidebar isOpen={isOpen} toggle={toggle}/>
             <Navbar toggle={toggle}/>
-         
+            <HeroSection/>
+            <InfoSection {...homeObjOne}/>
         </>
     );
 };
